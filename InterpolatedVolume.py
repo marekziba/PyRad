c3308c11a54deb6bd0ebb2f0b517745682906695
marek <marek.zieba@icloud.com>
2020-10-10 03:30:17 +0200

    cosmetic changes

--- InterpolatedVolume.py
+++ InterpolatedVolume.py
@@ -1,21 +1,21 @@
 from PolarVolume import PolarVolume
 import numpy as np
 
+
 class InterpolatedVolume:
     def __init__(self, volume: PolarVolume, hmin: float, hmax: float, vres: float):
         self.__hmin = hmin
         self.__hmax = hmax
         self.__vres = vres
-        nlevs = int((hmax - hmin)/vres)
+        nlevs = int((hmax - hmin) / vres)
         nrays, nbins = volume.getDimensions()
         dims = (nlevs, nrays, nbins)
         self.data = np.full(dims, 0.0)
         self.__levels = []
         for i in range(nlevs):
-            h = i * vres * 1000     #converting from km to meters
-            self.data[i, :, :] = volume.getCappi(h,pseudo=False)
+            h = i * vres * 1000  # converting from km to meters
+            self.data[i, :, :] = volume.getCappi(h, pseudo=False)
             self.__levels.append(h)
-        #print(self.__levels)
         self.data[np.isnan(self.data)] = -999.0
 
     def __validateProductBounds(self, bounds):
@@ -41,7 +41,6 @@ class InterpolatedVolume:
         stopIdx = len(self.__levels) - next(x for x, val in enumerate(reversed(self.__levels)) if val <= hmax)
         return (startIdx, stopIdx)
 
-
     def getCmax(self, bounds=None, absoluteMax=True):
         startIdx, stopIdx = self.__getIndexes(bounds)
         data = self.data.copy()
@@ -50,9 +49,9 @@ class InterpolatedVolume:
         if not absoluteMax:
             cmax = np.max(data[startIdx:stopIdx], axis=0)
         else:
-            max = np.max(data[startIdx:stopIdx], axis=0)
-            min = np.min(data[startIdx:stopIdx], axis=0)
-            cmax = np.where(max < abs(min), min, max)
+            maxArray = np.max(data[startIdx:stopIdx], axis=0)
+            minArray = np.min(data[startIdx:stopIdx], axis=0)
+            cmax = np.where(maxArray < abs(minArray), minArray, maxArray)
         cmax[cmax == 0.0] = np.nan
         return cmax
 
@@ -61,8 +60,8 @@ class InterpolatedVolume:
         data = self.data.copy()
         data = data[startIdx:stopIdx]
         vil = ((10 ** (data / 10)) / 24000) ** (1 / 1.82)
-        nodata = ((10 ** (-999.0 / 10)) / 24000) ** (1 / 1.82)
-        vil[vil == nodata] = 0.0
+        noData = ((10 ** (-999.0 / 10)) / 24000) ** (1 / 1.82)
+        vil[vil == noData] = 0.0
         vil = np.sum(vil, axis=0)
         vil[vil == 0.0] = np.nan
         return vil
